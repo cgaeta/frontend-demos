0568f70bf94b3cace2bd43fe065802c1f256e86e
cgaeta <chrismgaeta@gmail.com>
2021-07-22 10:22:20 -0400

    [Meme]: Add async increment button to Zustand Counter

--- src/features/ZustandCounter/ZustandCounter.tsx
+++ src/features/ZustandCounter/ZustandCounter.tsx
@@ -4,25 +4,54 @@ import create from 'zustand';
 
 import 'antd/dist/antd.css';
 
+enum LoadState {
+  idle = "idle",
+  loading = "loading",
+};
 type ZustandStore = {
   counter: number,
+  loadState: LoadState,
   increment: () => void,
-}
+  asyncIncrement: () => void,
+};
+
+const memeRequest = () => new Promise((resolve) =>
+  setTimeout(() => resolve(null), 500)
+);  
+
 const useStore = create<ZustandStore>((set) => ({
   counter: 0,
-  increment: () => set((state) => ({ counter: state.counter + 1 }))
+  loadState: LoadState.idle,
+  increment: () => set((state) => ({ counter: state.counter + 1 })),
+  asyncIncrement: async () => {
+    set((state) => ({ loadState: LoadState.loading }));
+    await memeRequest();
+    set((state) => ({
+      counter: state.counter + 1,
+      loadState: LoadState.idle,
+    }));
+  },
 }));
 
 const ZustandCounter = () => {
-  const { counter, increment } = useStore((state) =>
-    ({ counter: state.counter, increment: state.increment })
-  );
+  const {
+    counter,
+    loadState,
+    increment,
+    asyncIncrement,
+  } = useStore((state) => state);
 
   return (
     <div>
       <div>{counter}</div>
       <div>
         <AntdButton type="primary" onClick={increment}>Antd Button</AntdButton>
+        <AntdButton
+          onClick={asyncIncrement}
+          disabled={loadState === LoadState.loading}
+        >
+          Async
+        </AntdButton>
       </div>
     </div>
   );
