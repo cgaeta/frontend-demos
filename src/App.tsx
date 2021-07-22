85f2ec73509ac10712f061ff624a07c9ff40668f
cgaeta <chrismgaeta@gmail.com>
2021-07-22 10:55:57 -0400

    [Meme]: Add async button to state/context

--- src/App.tsx
+++ src/App.tsx
@@ -17,11 +17,21 @@ import './App.css';
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const increment = useCallback(() => setCounter(counter + 1), [counter, setCounter])
+  const increment = useCallback(() => setCounter(counter + 1), [counter, setCounter]);
+  const asyncIncrement = useCallback(async () => {
+    await new Promise((r) => setTimeout(() => r(null), 500));
+    setCounter((c) => c + 1);
+  }, [setCounter]);
   return (
     <RecoilRoot>
       <AppProvider i18n={enTranslations}>
-        <CounterContext.Provider value={{ counter, increment }}>
+        <CounterContext.Provider
+          value={{
+            counter,
+            increment,
+            asyncIncrement,
+          }}
+        >
           <div className="App">
             <header className="App-header">
               <Row>
--- src/features/ContextCounter/ContextCounter.tsx
+++ src/features/ContextCounter/ContextCounter.tsx
@@ -6,19 +6,22 @@ import '@shopify/polaris/dist/styles.css';
 export type CounterContextType = {
   counter: number,
   increment: () => void,
+  asyncIncrement: () => void,
 };
 export const CounterContext = createContext<CounterContextType>({
   counter: 0,
   increment: () => {},
+  asyncIncrement: async () => {},
 });
 
 const ContextCounter = () => {
-  const { counter, increment } = useContext(CounterContext);
+  const { counter, increment, asyncIncrement } = useContext(CounterContext);
   return (
     <div>
       <div>{counter}</div>
       <div>
         <PolarisButton onClick={increment}>Polaris Button</PolarisButton>
+        <PolarisButton outline onClick={asyncIncrement}>Async</PolarisButton>
       </div>
     </div>
   );
